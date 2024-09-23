0107cd9aee6fe0a42fdaedf605d9c3f578dad3d8
Sean Murphy <sean@iamseanmurphy.com>
2024-09-23 01:20:16 +0000

    1.6.2

---
+++ ga-germanized/ga-germanized.php
@@ -0,0 +1,234 @@
+<?php
+/*
+Plugin Name: Analytics Germanized for Google Analytics
+Plugin URI: https://wordpress.org/plugins/ga-germanized/
+Description: Google Analytics preconfigured to respect EU law (GDPR / DSGVO) and with lots of advanced analytics settings for extensive tracking possibilities.
+Version: 1.6.2
+Author: Pascal Bajorat
+Author URI: https://www.bajorat-media.com/
+Text Domain: ga-germanized
+Domain Path: /lang
+License: GNU General Public License v.3
+
+Copyright (c) 2022 by Bajorat-Media.com.
+*/
+
+/* Security-Check */
+if ( !class_exists('WP') ) {
+	die();
+}
+
+/* Constants */
+define('PBGAG_FILE', __FILE__);
+define('PBGAG_DIR', dirname(__FILE__));
+define('PBGAG_BASE', plugin_basename(__FILE__));
+define('PBGAG_VERSION', '1.6.2');
+
+if( ! class_exists('gaGermanized') ):
+
+	class gaGermanized
+	{
+		public static function init()
+		{
+			/*
+             * Language file
+             */
+			load_plugin_textdomain(
+				'ga-germanized',
+				false,
+				dirname( PBGAG_BASE ) . '/lang/'
+			);
+
+			/*
+			 * Admin functions
+			 */
+			add_action(
+				'init',
+				array(
+					'gag_settings_handler',
+					'init'
+				)
+			);
+
+			if( is_admin() ) {
+
+				add_action(
+					'admin_enqueue_scripts',
+					array(
+						'gag_settings',
+						'admin_enqueue_scripts'
+					)
+				);
+
+				add_action(
+					'admin_menu',
+					array(
+						'gag_settings',
+						'options_page_menu'
+					)
+				);
+
+				add_filter(
+					'plugin_action_links_'.PBGAG_BASE,
+					array(
+						'gag_settings',
+						'plugin_action_links'
+					)
+				);
+
+				try {
+                    new pbcockpitnotice([
+                        'logo'              => plugins_url(dirname(PBGAG_BASE)).'/assets/img/bajorat-media-white.svg',
+                        'link_de'           => 'https://cockpit.bajorat-media.com/infos/?utm_source=ga-germanized&utm_medium=banner&utm_campaign=ga-germanized',
+                        'link_en'           => 'https://cockpit.bajorat-media.com/en/infos/?utm_source=ga-germanized&utm_medium=banner&utm_campaign=ga-germanized',
+                        'only_on_screens'   => true,
+                        'allowed_screens'   => array('settings_page_ga-germanized')
+                    ]);
+                } catch (Exception $e) {}
+
+			} else { // if( is_admin() )
+
+				/*
+				 * Frontend functions
+				 */
+
+				add_action(
+					'wp_head',
+					array(
+						'gag_analytics',
+						'wp_head_code'
+					)
+				);
+
+				add_action(
+					'wp_enqueue_scripts',
+					array(
+						'gag_analytics',
+						'gag_tracker_js'
+					)
+				);
+
+                add_action(
+                    'init',
+                    array(
+                        'gag_cookieconsent',
+                        'init'
+                    )
+                );
+
+				add_action(
+					'wp_head',
+					array(
+						'gag_analytics',
+						'custom_tracker_head'
+					),
+					99
+				);
+
+				add_action(
+					'wp_footer',
+					array(
+						'gag_analytics',
+						'custom_tracker_footer'
+					),
+					99
+				);
+
+			} // end else if( is_admin() )
+
+			add_action(
+				'init',
+				array(
+					'gag_shortcodes',
+					'init'
+				)
+			);
+		}
+
+		public static function detect_plugin_activation( $plugin, $network_activation )
+		{
+			if( $plugin !== PBGAG_BASE || $network_activation ) {
+				return;
+			}
+
+			$settings_page_url = admin_url('options-general.php?page=ga-germanized');
+
+			if ( wp_redirect( $settings_page_url ) ) {
+				exit;
+			}
+		}
+
+		public static function activation_hook()
+		{
+			// no setup required
+		}
+
+		public static function uninstall_hook()
+		{
+			gag_settings_handler::delete_options();
+		}
+	}
+
+endif;
+
+add_action(
+	'plugins_loaded',
+	array(
+		'gaGermanized',
+		'init'
+	)
+);
+
+add_action(
+	'activated_plugin',
+	array(
+		'gaGermanized',
+		'detect_plugin_activation'
+	),
+	10,
+	2
+);
+
+register_activation_hook(
+	__FILE__,
+	array(
+		'gaGermanized',
+		'activation_hook'
+	)
+);
+
+register_uninstall_hook(
+	__FILE__,
+	array(
+		'gaGermanized',
+		'uninstall_hook'
+	)
+);
+
+/* Autoload Init */
+spl_autoload_register('pbgag_autoload');
+
+/* Autoload Function */
+function pbgag_autoload($class)
+{
+	$allowed_classes = array(
+		'gag_settings',
+		'gag_settings_handler',
+		'gag_analytics',
+		'gag_cookieconsent',
+		'gag_shortcodes',
+        'pbcockpitnotice'
+	);
+
+	if( in_array($class, $allowed_classes) ) {
+
+		$require_once = sprintf(
+			'%s/inc/%s.php',
+
+			PBGAG_DIR,
+			strtolower($class)
+		);
+
+		require_once( $require_once );
+	}
+}
---
+++ ga-germanized/inc/tpl/about-us.php
@@ -0,0 +1,40 @@
+<div class="gag-settings-item">
+    <?php if( get_locale() == 'de_DE_formal' || get_locale() == 'de_DE' ): ?>
+    <div class="gag-video-container">
+        <iframe width="1920" height="1080" src="https://www.youtube-nocookie.com/embed/SHfr_O6njJQ?rel=0&amp;hl=de&amp;modestbranding=1&amp;showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
+    <p style="text-align: center"><a href="https://cockpit.bajorat-media.com/infos/?utm_source=ga-germanized&utm_medium=logo&utm_campaign=ga-germanized" target="_blank"><img src="<?php echo plugins_url(dirname(PBGAG_BASE)) ?>/assets/img/bajorat-media.png" width="250" height="59" alt="Bajorat Media" /></a></p>
+
+    <h3>Seit mehr als 10 Jahren Ihre Full-Service-Webagentur</h3>
+    <p>Die Bajorat Media wurde im November 2006 in Lage (Lippe) gegründet und betreut seither kleine und große Kunden. Von Digitalisierungs-Projekten von KMU über die Entwicklung von E-Commerce Projekten für junge Startups - wir sind Ihr professioneller Ansprechpartner.</p>
+    <p>Seit 2007 betreuen wir vor allem Kunden in Berlin / Brandenburg sowie allen großen Medien- &amp; Tech-Metropolen in Deutschland. Gleichzeitig durften wir auch überregionale und internationale Partner aus gefühlt jedem Winkel der Welt beraten sowie Lösungen anbieten und umsetzen.</p>
+    <p>Egal, ob Berlin oder San Francisco Bay Area, seit mehr als 10 Jahren vertrauen unsere Kunden und Partner unseren Lösungen sowie der professionellen Beratung in den Bereichen WordPress, Webentwicklung, Webdesign und Marketing.</p>
+
+    <p><strong>WordPress-Agentur Bajorat Media</strong></p>
+    <p>Wir haben uns auf umfassende Full-Service Lösungen im Bereich Webdesign und Entwicklung spezialisiert. Dabei bieten wir Ihnen gerne ein umfangreiches Komplettpaket sowie auf Ihre Bedürfnisse zugeschnittene Einzelleistungen an.</p>
+    <p>WordPress ist das meistverwendete CMS der Welt. Seit etwas mehr als 10 Jahren haben auch wir uns im technischen Bereich voll und ganz auf das beliebte Open-Source CMS spezialisiert und gehören in dem Bereich zu den führenden und mehrfach ausgezeichneten WordPress-Agenturen.</p>
+
+    <p><a href="https://cockpit.bajorat-media.com/infos/?utm_source=ga-germanized&utm_medium=link&utm_campaign=ga-germanized" target="_blank"><strong>Möchten Sie mit uns zusammenarbeiten? Erzählen Sie uns von Ihrem Projekt!</strong></a></p>
+
+    <?php else: ?>
+
+    <div class="gag-video-container">
+        <iframe width="1920" height="1080" src="https://www.youtube-nocookie.com/embed/SHfr_O6njJQ?rel=0&amp;hl=en&amp;modestbranding=1&amp;showinfo=0&amp;cc_load_policy=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
+    <p style="text-align: center"><a href="https://cockpit.bajorat-media.com/en/infos/?utm_source=ga-germanized&utm_medium=logo&utm_campaign=ga-germanized" target="_blank"><img src="<?php echo plugins_url(dirname(PBGAG_BASE)) ?>/assets/img/bajorat-media.png" width="250" height="59" alt="Bajorat Media" /></a></p>
+
+    <h3>Your full-service web agency for more than 10 years</h3>
+    <p>Bajorat Media was founded in Lage (Lippe) in November 2006 and has been supervising projects for small, middle-sized and lanrge company customers. Digital projects by SME or developing e-commerce projects for young startups - we are your professional contact.</p>
+    <p>Since 2007 we are supporting customers in Berlin and Brandenburg in particular. At the same time we were able to consult, offer and implement solutions to national and international partners all over the world.</p>
+    <p>Whether Berlin or San Francisco Bay Area, for over 10 years our customers and partners trust our solutions and professional advice in the fields of WordPress, web development, web design and marketing.</p>
+
+    <p><strong>WordPress Agency Bajorat Media</strong></p>
+    <p>We specialize in complete full-service solutions in the field of web design and development. Our services will definitely meet your needs.</p>
+    <p>WordPress is the most common used CMS in the world. Specialized in the technical area of ​​the popular open source CMS we belong to the leading WordPress agencies which has received multiple awards.</p>
+    <p><a href="https://cockpit.bajorat-media.com/en/infos/?utm_source=ga-germanized&utm_medium=link&utm_campaign=ga-germanized" target="_blank"><strong>Request your project now!</strong></a></p>
+    <?php endif; ?>
+
+	<p style="text-align: center; margin: 30px 0 0 0;"><a href="https://www.bajorat-media.com/" target="_blank"><img src="<?php echo plugins_url(dirname(PBGAG_BASE)) ?>/assets/img/erecht24-siegel-agenturpartner.png" width="120" height="156" alt="" /></a></p>
+</div>
